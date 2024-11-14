ead391e3ced8e8f0271633156a2f3d818a62bf64
Gokhan Mutlu <gokhanmutlu04@yahoo.com>
2024-11-14 08:11:54 +0300

    Initial Commit

---
+++ VideoPLayer/VideoPLayerApp.swift
@@ -0,0 +1,12 @@
+// VideoPLayerApp.swift by Gokhan Mutlu on 14.11.2024
+
+import SwiftUI
+
+@main
+struct VideoPLayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
