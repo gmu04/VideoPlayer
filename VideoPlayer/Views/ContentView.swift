1297eaffc974f1424ad9f16e84b12cf5083ca428
Gokhan Mutlu <gokhanmutlu04@yahoo.com>
2024-11-14 08:49:11 +0300

    *

--- VideoPlayer/Views/ContentView.swift
+++ VideoPlayer/Views/ContentView.swift
@@ -10,20 +10,21 @@ struct ContentView: View {
 	
 	var body: some View {
 		ZStack {
-			Color.black.ignoresSafeArea()
+			Color.black
+				.ignoresSafeArea()
 			VStack {
 				//create a video-player user interface for the player object (2/3)
 				VideoPlayer(player: player)
 					.frame(maxWidth: .infinity)
-				.frame(height: 220)
+					.frame(height: 220)
+					.onAppear{
+						//play the video (3/3)
+						player.seek(to: CMTime.zero)
+						player.play()
+					}
 			}
-	
 		}
-		.onAppear{
-			//play the video (3/3)
-			player.seek(to: CMTime.zero)
-			player.play()
-		}
+		
 	}
 }
 
